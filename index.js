b9677056a0a79ee18254baefb4df66a205d32da2
Muhammad Khubaib <muhammadkhubaib492@gmail.com>
2024-06-22 15:42:53 +0500

    added col and row in web with bootstrap

--- index.js
+++ index.js
@@ -20,20 +20,24 @@ const blogs = document.getElementById('blogsProfile');
 
 BlogsProfieContent.map(blog => {
     const profileHTML = `
-    <div class="container blogs" id="${blog.id}" >
-        <img class="blogImg" src="${blog.imageSrc}" alt="">
-        <div class="content" >
-            <h1>${blog.title}</h1>
-            <hr>
-            <p>Industry:  <b> Events and Tourism </b></p>
-            <p>Skills: <b>  Figma, PHP, Angular, TypeScript, RXJS, MYSQL, Firebase, Postman </b></p>
-
-            <div class="btnSec conatiner">
-                <button>Read More</button>
-                <a href="">Live view <img src="arrow_outward.png" alt=""></a>
+    <div class="container blogs"> 
+    <div class="row" id="${blog.id}">
+            <img class="blogImg img-fluid col-12 col-md-6" src="${blog.imageSrc}" alt="">
+        <div style="padding-left:0rem" class="col-12 col-md-6">
+            <div class="content">
+                <h1>${blog.title}</h1>
+                <hr>
+                <p>Industry: <b>Events and Tourism</b></p>
+                <p>Skills: <b>Figma, PHP, Angular, TypeScript, RXJS, MYSQL, Firebase, Postman</b></p>
+                <div class="btnSec">
+                    <button>Read More</button>
+                    <a href="">Live view <img src="arrow_outward.png" alt=""></a>
+                </div>
             </div>
         </div>
     </div>
+</div>
+
             `;
     blogs.innerHTML += profileHTML;
 });
